47b4b34ae9333162b1363e326829a2024fbdc0cf
opilane <opilane@HP-CZC6297B8C>
2021-02-23 10:32:41 +0200

    12

--- Zmeika.Koršun/Sounds.cs
+++ Zmeika.Koršun/Sounds.cs
@@ -8,6 +8,34 @@ namespace Zmeika.Koršun
 {
     public class Sounds
     {
+        WindowsMediaPlayer=new WindowsMediaPlayer();
+        private string pathToMedia;
 
+        public Sounds(string pathToResources)
+        {
+            pathToMedia = pathToResources;
+        }
+
+        public void Play()
+        {
+            player.URL = pathToMedia + "stardust.mp3";
+            player.settings.volume = 30;
+            player.controls.play();
+            player.settings.setMode("loop", true);
+
+        }
+
+        public void Play(string songName)
+        {
+            player.URL = pathToMedia + songName + ".mp3";
+            player.controls.play();
+        }
+
+        public void PlayEat()
+        {
+            player.URL = pathToMedia + "click.mp3";
+            player.settings.volume = 100;
+            player.controls.play();
+        }
     }
 }
