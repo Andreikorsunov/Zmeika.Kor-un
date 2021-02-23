adce3bf65d1fea703473b86eee9bd268d27d4cae
opilane <opilane@HP-CZC6297B8C>
2021-02-23 10:40:42 +0200

    as

---
+++ Zmeika.Koršun/Start.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmeika.Koršun
+{
+    class Start
+    {
+        public ConsoleKeyInfo key;
+
+        public Start()
+        {
+
+        }
+        int valik = 0;
+
+        public int choice()
+        {
+            Console.WriteLine("Start game -\nStop game - q")
+        }
+    }
+}
